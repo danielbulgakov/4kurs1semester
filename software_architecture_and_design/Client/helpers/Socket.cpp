b835482b16966c8fec3d567a0f44ab38ac8e879a
danielbulgakov <bulgach09@gmail.com>
2023-09-23 03:09:29 +0300

    TASK: Send mes in client while not sent

--- software_architecture_and_design/Client/helpers/Socket.cpp
+++ software_architecture_and_design/Client/helpers/Socket.cpp
@@ -47,7 +47,20 @@ int Socket::send(const char* msg, int len)
     if(len <= 0)
         return 0;
     printf("SEND\n----------\n%s\n----------\n", msg);
-    return ::send(m_socket, msg, len, 0);
+
+    int bytesSent = 0;
+    int result = 0;
+
+    while (bytesSent < len) {
+        result = ::send(m_socket, msg + bytesSent, len - bytesSent, 0);
+        if (result == -1) {
+            bytesSent = -1; // Error value to return from fun
+            break;
+        }
+        bytesSent += result;
+    }
+
+    return bytesSent;
 }
 
 int Socket::sendStr(const std::string& str)
